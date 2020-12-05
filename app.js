4c251bf88776442e3bd708f215827d3542c3033f
magarrutuja <72261078+magarrutuja@users.noreply.github.com>
2020-12-06 01:34:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-console.log(txt-input)
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  return serverURL + "?" + "text=" + text;
+}
 
 function clickHandler() {
-    console.log("clicked!");
-    console.log("input",txtInput.value);
+  var inputText = txtInput.value;
 
-};
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
 
-btnTranslate.addEventListener("click", clickHandler)
-  
+btnTranslate.addEventListener("click", clickHandler);
